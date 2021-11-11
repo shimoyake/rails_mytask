69051fc5d5f3e01cdcb7f558b0a781635cc386df
EC2 Default User <ec2-user@ip-172-31-10-254.us-east-2.compute.internal>
2021-11-11 08:32:44 +0000

    ユーザー画像登録OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   
   def profile
     @user = User.find_by(id: current_user.id)
+    #@user = User.find_by(id: params[:id])
   end
   
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :rooms
+  
+  mount_uploader :image, ImageUploader
 end
